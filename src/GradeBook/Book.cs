1a5e1fbf82fade50549a61018b930d563ab7dacd
Jason Hitching <jasonhitching1994@gmail.com>
2022-08-16 20:44:40 +0100

    <refactored> statistical computations into 'book.cs'

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,21 +1,43 @@
-namespace GradeBook {
-    public class Book {
+namespace GradeBook
+{
+    public class Book
+    {
 
         private List<double> grades;
         private string name;
+        private double total = 0.0, average = 0.0;
+        private double highGrade = double.MinValue;
+        private double lowGrade = double.MaxValue;
 
         // Constructor
-        public Book(string name) {
+        public Book(string name)
+        {
             this.name = name;
             grades = new List<double>();
         }
 
-        public void AddGrade(double newGrade) {
+        public void AddGrade(double newGrade)
+        {
             grades.Add(newGrade);
         }
 
-        public string GetName() {
+        public string GetName()
+        {
             return this.name;
+        }
+
+        public string ShowStatistics()
+        {
+            foreach (double grade in grades)
+            {
+                lowGrade = Math.Min(grade, lowGrade);
+                highGrade = Math.Max(grade, highGrade);
+                total += grade;
+            }
+
+            return $"The lowest grade is {lowGrade}\n"
+                 + $"The highest grade is {highGrade}\n"
+                 + $"The average grade is {total / grades.Count}";
         }
 
     }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,24 +1,18 @@
 ﻿
-namespace GradeBook {
-    class Program {
-        static void Main(string[] args) {
-            
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
             Book book = new Book("Jason's Book");
-
             book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            string stats = book.ShowStatistics();
 
-            var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
-
-            double total = 0.0, average = 0.0;
-            
-            foreach (var grade in grades) {
-                total += grade;
-            } 
-
-            average = total / grades.Count;
-
-            Console.WriteLine($"The total sum of grades is {total}");
-            Console.WriteLine($"The average grades is {average:N1}");
+            Console.WriteLine(stats);
         }
     }
 }
