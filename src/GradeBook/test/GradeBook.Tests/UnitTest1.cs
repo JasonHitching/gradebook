7d9535691d8145853c019235400410bb5557e8c2
Jason Hitching <jasonhitching1994@gmail.com>
2022-08-16 20:54:15 +0100

    <implemented dummy tests

--- src/GradeBook/test/GradeBook.Tests/UnitTest1.cs
+++ src/GradeBook/test/GradeBook.Tests/UnitTest1.cs
@@ -6,6 +6,15 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        // arrange
+        int x = 5;
+        int y = 2;
+        int expected = 7;
 
+        // act
+        int actual = x * y;
+
+        // assert
+        Assert.Equal(expected, actual);
     }
 }
